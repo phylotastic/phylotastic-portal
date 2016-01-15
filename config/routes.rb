9be3e2dfe6c5935972a23b362ad21325e09f2573
ndvan <vannd@vccloud.vn>
2016-01-15 14:36:51 -0700

    devise for authenticating users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
+  root 'static_pages#home'
   get 'static_pages/about'
-
   get 'static_pages/faq'
 
   # The priority is based upon order of creation: first created -> highest priority.
