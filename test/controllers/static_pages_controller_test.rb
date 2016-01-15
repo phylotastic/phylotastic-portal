4344767fc7c8cb6cc67e626e88c06cfb30b0e396
ndvan <vannd@vccloud.vn>
2016-01-15 11:50:22 -0700

    automate test with Guard, few first tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Phylotastic Web Portal"
+  end
+  
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 
   test "should get faq" do
     get :faq
     assert_response :success
+    assert_select "title", "FAQ | #{@base_title}"
   end
 
 end
