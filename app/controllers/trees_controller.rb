d3e58d404a3b82261c9db10a9a7a455c2de9800c
ndvan <vannd@vccloud.vn>
2016-01-26 07:27:14 -0700

    add styles for tree getter

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -3,8 +3,22 @@ class TreesController < ApplicationController
   
   def new
     @tree = current_user.trees.build
+    @species = ["Admiral", "Ackbar", "Padm", "Amidala", "Wedge", "Antilles", "Cad", "Bane", "BB-8", "Darth", "Bane", "Jar", "Jar", "Binks", "C-3PO", "Lando", "Calrissian", "Chewbacca", "Poe", "Dameron", "Count", "Dooku", "(Darth", "Tyranus)", "Boba", "Fett", "Jango", "Fett", "Finn", "Greedo", "General", "Grievous", "Jabba", "the", "Hutt", "Qui-Gon", "Jinn", "Maz", "Kanata", "Obi-Wan", "Kenobi", "Princess", "Leia", "Darth", "Maul", "Palpatine", "(Darth", "Sidious)", "Captain", "Phasma", "R2-D2", "Rey", "Anakin", "Skywalker", "(Darth", "Vader)", "Luke", "Skywalker", "Kylo", "Ren", "Han", "Solo", "Ahsoka", "Tano", "Grand", "Moff", "Wilhuff", "Tarkin", "Asajj", "Ventress", "Watto", "Mace", "Windu", "Yoda"]
   end
   
   def create
+    binding.pry
+    @tree = current_user.trees.build(tree_params)
+    if @tree.save
+      flash[:success] = "Tree created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
   end
+  
+  private
+    def tree_params
+      params.require(:tree).permit(:phylo_source_id, :branch_length, :images_from_EOL)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123133640) do
+ActiveRecord::Schema.define(version: 20160125203252) do
 
   create_table "phylo_sources", force: :cascade do |t|
     t.string   "name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20160123133640) do
     t.integer  "phylo_source_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "branch_length"
+    t.boolean  "images_from_EOL"
   end
 
   add_index "trees", ["phylo_source_id"], name: "index_trees_on_phylo_source_id"
