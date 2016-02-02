ca04b7256f156495ff07f74ed028dc117c007603
ndvan <ducvan0212@gmail.com>
2016-02-01 18:02:09 -0700

    show errors when submit blank fields

--- app/controllers/con_links_controller.rb
+++ app/controllers/con_links_controller.rb
@@ -1,5 +1,20 @@
 class ConLinksController < ApplicationController
   def create
-    binding.pry
+    @con_link = current_user.con_links.build(con_link_params)
+    if @con_link.save
+      flash[:success] = "Processing URL!"
+      TreesWorker.perform_async(@con_link.id, "ConLink")
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Could not process URL"
+      @con_file = ConFile.new
+      render 'raw_extractions/new_from_file_and_web'
+    end
   end
+  
+  private
+    def con_link_params
+      params.require(:con_link).permit(:uri)
+    end
+    
 end
--- app/controllers/raw_extractions_controller.rb
+++ app/controllers/raw_extractions_controller.rb
@@ -1,4 +1,6 @@
 class RawExtractionsController < ApplicationController
   def new_from_file_and_web
+    @con_link = ConLink.new
+    @con_file = ConFile.new
   end
 end
