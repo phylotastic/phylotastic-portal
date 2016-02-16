dfa14b60cb99d595eb85d0910f5230c3a59eb733
ndvan <ducvan0212@gmail.com>
2016-02-16 14:57:09 -0700

    add config and change default value in select box in tree getter

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -40,11 +40,12 @@ class TreesController < ApplicationController
   end
   
   def update
+    binding.pry
     @tree = current_user.trees.find_by_id(params[:id])
     if @tree.update_attributes(tree_params)
       flash[:success] = "Tree ##{params[:id]} is under constructed. We will notify you when it is ready"
       job_id = TreesWorker.perform_async(@tree.id)
-      @tree.update_attributes(bg_job: job_id)  
+      @tree.update_attributes(bg_job: job_id, status: "constructing")
       redirect_to trees_path
     else
       render 'edit'
