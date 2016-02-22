e154ed6e4e0cbdfb95080bb8a1f12452eb2b22d0
ndvan <ducvan0212@gmail.com>
2016-02-22 00:23:16 -0700

    change migration order

--- db/migrate/20160220010039_create_subset_taxons.rb
+++ db/migrate/20160220010039_create_subset_taxons.rb
@@ -3,7 +3,6 @@ class CreateSubsetTaxons < ActiveRecord::Migration
     create_table :subset_taxons do |t|
       t.boolean :has_genome_in_ncbi
       t.references :user, index: true, foreign_key: true
-      t.references :country, index: true, foreign_key: true
     end
   end
 end
