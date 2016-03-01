d5e53ae23d1e0117d8d442ed11cb197ee15c8ff8
ndvan <vannd@vccloud.vn>
2016-03-01 14:58:32 -0700

    integrate with facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook, :twitter]
+         :omniauthable, :omniauth_providers => [:facebook]
   
   has_many :trees, dependent: :destroy
   has_many :con_files, dependent: :destroy
