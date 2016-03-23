e24ffe6f89d7e486bd4e464ddf0df3b2b94dffd8
ndvan <vannd@vccloud.vn>
2016-03-23 00:40:54 -0600

    read sheet files

--- app/models/uploaded_list.rb
+++ app/models/uploaded_list.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'roo'
+
 class UploadedList < ActiveRecord::Base
   belongs_to :user
   
@@ -5,4 +8,26 @@ class UploadedList < ActiveRecord::Base
   validates_attachment :file, presence: true,
     content_type: { content_type: "application/zip", :message => ": Make sure to zip your files" },
     size: { in: 0..500.kilobytes, :message => "must be less than 500kB" }
+    
+  def self.process(file)
+    spreadsheet = self.open_spreadsheet(file)
+    header = spreadsheet.row(1)
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      puts row
+    end
+  end
+
+  def self.open_spreadsheet(file)
+    case File.extname(File.basename(file))
+    when ".csv" then Roo::Csv.new(file.path)
+    when ".xls" then Roo::Excel.new(file.path)
+    when ".xlsx" then Roo::Excelx.new(file.path)
+    else raise "Unknown file type: #{File.basename(file)}"
+    end
+  end
+  
+  def self.extraction_location(f_path, entry_name)
+    File.dirname(f_path) + "/#{entry_name}"
+  end
 end
