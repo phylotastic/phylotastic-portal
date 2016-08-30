7fed93cdad071fa875452693dcba012c9b1cd973
ndvan <vannd@vccloud.vn>
2016-08-30 11:53:13 -0600

    newick download

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -182,7 +182,7 @@ class TreesController < ApplicationController
     if @tree.nil?
       redirect_to root_path
     else 
-      send_data @tree.representation, :filename => @tree.name + "_newick.txt"
+      send_data JSON.parse(@tree.representation)["newick"], :filename => @tree.name + "_newick.txt"
     end
   end
   
