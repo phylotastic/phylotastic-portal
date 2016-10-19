e0328ce1dc18cdd4682191877edf20b854d99213
ndvan <vannd@vccloud.vn>
2016-10-19 14:09:23 -0600

    filter uri first

--- app/models/con_link.rb
+++ app/models/con_link.rb
@@ -1,4 +1,6 @@
 class ConLink < ActiveRecord::Base
+  before_validation :smart_add_url_protocol
+
   belongs_to :user
 
   has_one :raw_extraction, as: :contributable, dependent: :destroy
@@ -7,10 +9,18 @@ class ConLink < ActiveRecord::Base
   
   validates :name, presence: true
   validates :user_id, presence: true
-  validates :uri, :presence => true, :url => true
-  
+  validates :uri, :presence => true, :url => true  
+
   def meta_origin
     uri
   end
-  
+
+  protected
+
+  def smart_add_url_protocol
+    unless self.uri[/^https?:\/\//]
+      self.uri = "http://#{self.uri}"
+    end
+  end
+    
 end
