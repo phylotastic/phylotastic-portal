a61fbdb5070b395a8def2558bf59272d9fdc857c
ndvan <vannd@vccloud.vn>
2018-04-18 23:19:43 -0600

    test static pages and create footer

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Phylotastic Portal"
   end
 
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Phylotastic Portal"
+  end
+  
+  test "should get feedback" do
+    get static_pages_feedback_url
+    assert_response :success
+    assert_select "title", "Feedback | Phylotastic Portal"
+  end
 end
