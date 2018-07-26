4ec7bc5433200d3f826fae4fe1fc10dcf46a0715
ndvan <vannd@vccloud.vn>
2018-07-26 10:50:34 -0600

    sidekiq config

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,30 @@
+require 'sidekiq'
+require 'sidekiq-status'
+
+Sidekiq.configure_client do |config|
+  config.client_middleware do |chain|
+    # accepts :expiration (optional)
+    chain.add Sidekiq::Status::ClientMiddleware, expiration: 30.minutes # default
+  end
+  if Rails.env == "production"
+    config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_app_name_#{Rails.env}" }
+  elsif Rails.env == "development"
+    config.redis = { url: 'redis://localhost:6379/1', namespace: "sidekiq_app_name_#{Rails.env}" }
+  end
+end
+
+Sidekiq.configure_server do |config|
+  config.server_middleware do |chain|
+    # accepts :expiration (optional)
+    chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
+  end
+  config.client_middleware do |chain|
+    # accepts :expiration (optional)
+    chain.add Sidekiq::Status::ClientMiddleware, expiration: 30.minutes # default
+  end
+  if Rails.env == "production"
+    config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_app_name_#{Rails.env}" }
+  elsif Rails.env == "development"
+    config.redis = { url: 'redis://localhost:6379/1', namespace: "sidekiq_app_name_#{Rails.env}" }
+  end
+end
