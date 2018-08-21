49790c47a37c75b48ea2f15c9e5709c61fdcb972
ndvan <vannd@vccloud.vn>
2018-08-21 06:52:58 -0600

    change email generator

--- test/load/jmeter_plan.rb
+++ test/load/jmeter_plan.rb
@@ -1,44 +1,51 @@
 require 'ruby-jmeter'
 
+# test do
+#
+#   defaults domain: 'phylo.cs.nmsu.edu'
+#
+#   cache clear_each_iteration: true
+#
+#   cookies
+#
+#   threads 5, {ramp_time: 60, duration: 120, continue_forever: true} do
+#
+#     random_timer 5000, 15000
+#
+#     # <meta name="csrf-token" content="Y/Ewo6stZRBSdI9SACiG0eHk/flggkPkBb7hHreT+ks34Ct4As5HI5afMtX0/bKhaNf/FX3/Uuo1lV+AEnePTw==">
+#     extract name: 'authenticity_token',
+#             regex: 'meta name="csrf-token" content="(.*)"'
+#
+#     # transaction '01_visit_home_page' do
+#       visit url: '/' do
+#         assert contains: 'Home | Phylotastic Portal'
+#       end
+#     # end
+#
+#     # transaction '02_create_list' do
+# #       submit '/links', {
+# #         fill_in: {
+# #           'utf8'               => '%E2%9C%93',
+# #           'authenticity_token' => '${__urlencode(${authenticity_token})}',
+# #           'link[url]'          => 'https://en.wikipedia.org/wiki/Muroidea',
+# #           "name"               => "Muroidea",
+# #           'commit'             => 'Submit'
+# #         }
+# #       }
+# #     end
+#   end
+#
+# end.run(
+#   path: '/usr/local/bin/',
+#   file: 'jmeter.jmx',
+#   log: 'jmeter.log',
+#   jtl: 'results.jtl',
+#   properties: 'jmeter.properties')
+
 test do
-
-  defaults domain: 'phylo.cs.nmsu.edu'
-
-  cache clear_each_iteration: true
-
-  cookies
-
-  threads 5, {ramp_time: 60, duration: 120, continue_forever: true} do
-
-    random_timer 5000, 15000
-
-    # <meta name="csrf-token" content="Y/Ewo6stZRBSdI9SACiG0eHk/flggkPkBb7hHreT+ks34Ct4As5HI5afMtX0/bKhaNf/FX3/Uuo1lV+AEnePTw==">
-    extract name: 'authenticity_token',
-            regex: 'meta name="csrf-token" content="(.*)"'
-
-    transaction '01_visit_home_page' do
-      visit '/' do
-        assert contains: 'Hello,'
-      end
+  threads count: 1 do
+    visit name: "Altentee", url: "http://phylo.cs.nmsu.edu" do
+      assert contains: "Home | Phylotastic Portal"
     end
-
-    transaction '02_create_list' do
-      submit '/links', {
-        fill_in: {
-          'utf8'               => '%E2%9C%93',
-          'authenticity_token' => '${__urlencode(${authenticity_token})}',
-          'link[url]'          => 'https://en.wikipedia.org/wiki/Muroidea',
-          "name"               => "Muroidea",
-          'commit'             => 'Submit'
-        }
-      }
-    end
-
   end
-
-end.run(
-  path: '/usr/local/bin/',
-  file: 'jmeter.jmx',
-  log: 'jmeter.log',
-  jtl: 'results.jtl',
-  properties: 'jmeter.properties')
+end.run
