922f4ab19e295a2c91896111ec3abff9443bf154
ndvan <vannd@vccloud.vn>
2019-10-08 14:44:31 -0600

    dockerize

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,29 @@
-FROM ruby:2.5.0
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs postgresql-client
-RUN mkdir /phylotastic-portal
-WORKDIR /phylotastic-portal
-COPY Gemfile /phylotastic-portal/Gemfile
-COPY Gemfile.lock /phylotastic-portal/Gemfile.lock
-RUN bundle install
-COPY . /phylotastic-portal
+# FROM ruby:2.5.0
+# RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs postgresql-client
+# RUN mkdir /phylotastic-portal
+# WORKDIR /phylotastic-portal
+# COPY Gemfile /phylotastic-portal/Gemfile
+# COPY Gemfile.lock /phylotastic-portal/Gemfile.lock
+# RUN bundle install
+# COPY . /phylotastic-portal
+#
+# CMD ["/phylotastic-portal/exe/startup.sh"]
 
-CMD ["/phylotastic-portal/exe/startup.sh"]
+
+FROM ruby:2.5
+RUN apt-get update -qq && apt-get install -y nodejs postgresql-client
+RUN mkdir /myapp
+WORKDIR /myapp
+COPY Gemfile /myapp/Gemfile
+COPY Gemfile.lock /myapp/Gemfile.lock
+RUN bundle install
+COPY . /myapp
+
+# Add a script to be executed every time the container starts.
+COPY entrypoint.sh /usr/bin/
+RUN chmod +x /usr/bin/entrypoint.sh
+ENTRYPOINT ["entrypoint.sh"]
+EXPOSE 3000
+
+# Start the main process.
+CMD ["rails", "server", "-b", "0.0.0.0"]
--- exe/startup.sh
+++ exe/startup.sh
@@ -1,45 +1,66 @@
 #!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
+# set -euo pipefail
+# IFS=$'\n\t'
+#
+# function wait_for_db {
+#   sleep 3
+#
+#   while [[ $(pg_isready -h "${PG_HOST}" \
+#            -U "${PG_USER}") = "no response" ]]; do
+#     echo "Waiting for postgresql to start..."
+#     sleep 1
+#   done
+# }
+#
+# function development {
+#   cd /phylotastic-portal
+#
+#   bundle exec rake db:drop
+#   bundle exec rake db:create
+#   bundle exec rake db:migrate RAILS_ENV=developement
+#
+#   rails s
+# }
+#
+# function production {
+#   # cd /app && npm run build && mkdir -p public
+#   # cp -r ./dist/* ./public && rm -R ./dist/*
+#
+#   bundle exec rake db:migrate
+#   rails s
+# }
+#
+# echo "Running 1\n"
+#
+# if [[ ! ${PG_HOST:?Requires PG_HOST} \
+#    || ! ${PG_USER:?Requires PG_USER} ]]; then
+#   echo "Need PG_HOST"
+#   exit 1
+# fi
+#
+# echo "Running 2\n"
+#
+# wait_for_db
+#
+# if [[ ${RAILS_ENV} = "production" ]]; then production; else development; fi
+
 
 function wait_for_db {
   sleep 3
 
-  while [[ $(pg_isready -h "${PG_HOST}" \
-           -U "${PG_USER}") = "no response" ]]; do
+  while [[ $(pg_isready -h "db" \
+           -U "postgres") = "no response" ]]; do
     echo "Waiting for postgresql to start..."
     sleep 1
   done
 }
 
-function development {
-  cd /phylotastic-portal
-
-  bundle exec rake db:drop
-  bundle exec rake db:create
-  bundle exec rake db:migrate
-  bundle exec rake db:migrate RAILS_ENV=test
-  
-  rails s
-}
-
-function production {
-  cd /app && npm run build && mkdir -p public
-  cp -r ./dist/* ./public && rm -R ./dist/*
-
-  bundle exec rake db:migrate
-  rails s
-}
-
-echo "Running 1\n"
-
-if [[ ! ${PG_HOST:?Requires PG_HOST} \
-   || ! ${PG_USER:?Requires PG_USER} ]]; then
-  exit 1
-fi
-
-echo "Running 2\n"
-
 wait_for_db
 
-if [[ ${RAILS_ENV} = "production" ]]; then production; else development; fi
+echo "Connected to db"
+
+cd /myapp
+
+RAILS_ENV=developement bundle exec rake db:drop
+RAILS_ENV=developement bundle exec rake db:create
+RAILS_ENV=developement bundle exec rake db:migrate
