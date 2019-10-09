1d57999585150de52c185b114a044fc96cd3cf85
ndvan <vannd@vccloud.vn>
2019-10-09 16:41:18 -0600

    dockerize the portal

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,8 +8,10 @@ Sidekiq.configure_client do |config|
   end
   if Rails.env == "production"
     config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_portal_#{Rails.env}" }
+    # config.redis = { url: 'redis://redis:6379/0', namespace: "sidekiq_portal_#{Rails.env}" }
   elsif Rails.env == "development"
     config.redis = { url: 'redis://localhost:6379/1', namespace: "sidekiq_portal_#{Rails.env}" }
+    # config.redis = { url: 'redis://redis:6379/1', namespace: "sidekiq_portal_#{Rails.env}" }
   end
 end
 
@@ -24,7 +26,9 @@ Sidekiq.configure_server do |config|
   end
   if Rails.env == "production"
     config.redis = { url: 'redis://localhost:6379/0', namespace: "sidekiq_portal_#{Rails.env}" }
+    # config.redis = { url: 'redis://redis:6379/0', namespace: "sidekiq_portal_#{Rails.env}" }
   elsif Rails.env == "development"
     config.redis = { url: 'redis://localhost:6379/1', namespace: "sidekiq_portal_#{Rails.env}" }
+    # config.redis = { url: 'redis://redis:6379/1', namespace: "sidekiq_portal_#{Rails.env}" }
   end
 end
